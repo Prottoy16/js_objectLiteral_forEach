7799149e3f8429a91987c62e1bf2ecb5666cedf4
Prottoy <95785154+Prottoy16@users.noreply.github.com>
2023-12-09 05:57:15 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
-// const display=(name,age)=>{
-//     return {
-//      name,
-//      age
-//     }
-// }
-// let bioData=display("Pinaky",23);
 
-// console.log(bioData);
-
-
-// const object1={
-//     name:"Prottoy",
-//     age:23,
-//     'display1 name'(){
-//         return `I am object function`
-//     }
-// }
-
-// console.log(object1.name);
-// console.log(object1.age);
-// console.log(object1['display1 name']());
-
-var text="";
-const arrayName=["Pinaky","Mehedi","Sakib"];
-arrayName.forEach(myFunction);
-console.log(arrayName);
-function myFunction(value){
-//   text= `${index} : ${value}`;
-    console.log(value);
+const numbers=[1,2,3,4,5,6];
+for(let number of numbers){
+    console.log(number);
 }
-const numbers=[1,2,3,4,5];
-const squarNumbers=[];
 
-numbers.forEach(function(value){
-   squarNumbers.push(value*value);
-});
-console.log(squarNumbers);
+const object1={
+    name : "Prottoy",
+    age : 23,
+    ID :2211587
+}
+
+for(let x in object1){
+    console.log(`${x} : ${object1[x]}`);
+}
